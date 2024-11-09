1ec8c2d2b7929b544632a77cd6f82a6d1a36edbe
Ysk-dev <yusufsertkayaysk@gmail.com>
2024-11-09 22:15:18 +0300

    bfs

--- time.py
+++ time.py
@@ -1,8 +1,11 @@
 # Python3 program for the above approach
-
 # Function to find a pair in the given
+
+
 # array whose sum is equal to z
 def findPair(a, n, z) :
+
+
 	
 	# Iterate through all the pairs
 	for i in range(n) :
@@ -28,4 +31,26 @@ if (findPair(a, n, z)) :
 else :
 	print("False")
 	
-	# This code is contributed by splevel62.
+ # type: ignore
+ 
+
+ #optimized version using hashset
+
+def find_pair(a, z):
+    seen = set()  # Initialize an empty set to keep track of elements we have seen
+    for num in a:
+        complement = z - num  # Calculate what we need to pair with `num` to get `z`
+        if complement in seen:  # If the complement is already in the set, we found a pair
+            return True
+        seen.add(num)  # Add the current number to the set
+    return False
+
+# Driver Code
+a = [1, -2, 1, 0, 5]
+z = 0
+
+# Function Call
+if findPair(a, z):
+    print("True")
+else:
+    print("False")
